c5fab432bdee0e499279693258bc78bd1c59868b
Jeff Hansen <jeffxhansen@gmail.com>
2024-06-15 14:52:26 -0600

    adjusting lightsOn.py to work from root

--- quick_scripts/lightsOn.py
+++ quick_scripts/lightsOn.py
@@ -5,5 +5,9 @@ x=[print(m) for m in sys.modules]
 print(os.getcwd())
 
 from pigpio_master import pigpio
+
+x=[print(m) for m in sys.modules]
+print(os.getcwd())
+
 pi = pigpio.pi()
 pi.set_PWM_dutycycle(4, 20)
